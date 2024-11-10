b07614b2df57e585e3d7cb33f228fc174b029393
Umbrella167 <1670187757@qq.com>
2024-11-10 17:01:39 +0800

    add- PlotVzBox TimedDeque

--- ui/Boxes/PlotVzBox.py
+++ ui/Boxes/PlotVzBox.py
@@ -1,19 +1,42 @@
 import dearpygui.dearpygui as dpg
-from ui.Boxes import Box
+from collections import deque
 import tbkpy._core as tbkpy
 import pickle
 import time
-from collections import deque
 
-class PlotVzBox(Box):
+# 定义 TimedDeque 类
+class TimedDeque:
+    def __init__(self, max_age_seconds):
+        self.deque = deque()
+        self.max_age_seconds = max_age_seconds
+    
+    def append(self, item):
+        # 添加元素和当前时间戳
+        self.deque.append((item, time.time()))
+        self._remove_old_items()
+    
+    def _remove_old_items(self):
+        # 移除超过时间限制的元素
+        current_time = time.time()
+        while self.deque and (current_time - self.deque[0][1]) > self.max_age_seconds:
+            self.deque.popleft()
+    
+    def __iter__(self):
+        # 迭代元素，忽略时间戳
+        return (item for item, _ in self.deque)
+
+    def __len__(self):
+        # 返回元素数量
+        return len(self.deque)
+
+class PlotVzBox:
     def __init__(self, uidata, tbkdata):
-        self
         self.subscriber_func = []
         self.subscription_data = {}
         self.is_axis_move = True
         self.data_start_time = time.time()
-        self.max_data_length = 500  # Set the maximum length for data storage
-
+        self.now_time = time.time()
+        self.max_save_time = 10  # 数据保存的最大时间限制
     def toggle_axis_move(self):
         self.is_axis_move = not self.is_axis_move
 
@@ -37,24 +60,21 @@ class PlotVzBox(Box):
             dpg.add_key_release_handler(key=dpg.mvKey_Spacebar, callback=self.toggle_axis_move)
 
     def subscriber_msg(self, msg, user_data):
-        series_tag, start_time = user_data
+        series_tag = user_data
         msg = pickle.loads(msg)
-        print(f"Received message: {msg}")
         if series_tag not in self.subscription_data:
             self.subscription_data[series_tag] = {
-                'time': deque(maxlen=self.max_data_length),
+                'time': TimedDeque(max_age_seconds=self.max_save_time),
                 'data': {}
             }
 
-            if isinstance(msg, (list, tuple)):
-                for i in range(len(msg)):
-                    self.subscription_data[series_tag]['data'][i] = deque(maxlen=self.max_data_length)
-                    dpg.add_line_series(x=[0], y=[0], label=f"{series_tag}_{i}", tag=f"{series_tag}_{i}", parent="plotvz_xaxis")
-            else:
-                self.subscription_data[series_tag]['data'][0] = deque(maxlen=self.max_data_length)
+            if not isinstance(msg, (list, tuple)):
+                msg = [msg]
+            for i in range(len(msg)):
+                self.subscription_data[series_tag]['data'][i] = TimedDeque(max_age_seconds=self.max_save_time)
+                dpg.add_line_series(x=[0], y=[0], label=f"{series_tag}_{i}", tag=f"{series_tag}_{i}_line", parent="plotvz_xaxis")
 
-        current_time = time.time() - start_time
-        self.subscription_data[series_tag]['time'].append(current_time)
+        self.subscription_data[series_tag]['time'].append(self.now_time - self.data_start_time)
 
         if not isinstance(msg, (list, tuple)):
             msg = [msg]
@@ -63,7 +83,7 @@ class PlotVzBox(Box):
             self.subscription_data[series_tag]['data'][i].append(value)
             if self.is_axis_move:
                 dpg.configure_item(
-                    item=f"{series_tag}_{i}",
+                    item=f"{series_tag}_{i}_line",
                     x=list(self.subscription_data[series_tag]['time']),
                     y=list(self.subscription_data[series_tag]['data'][i])
                 )
@@ -71,24 +91,22 @@ class PlotVzBox(Box):
     def plot_drop_callback(self, sender, app_data, user_data):
         _msg = app_data['msg']
         name = app_data['name']
-        series_tag = f'{_msg}:{name}_line'
+        series_tag = f'{_msg}:{name}'
 
         if series_tag not in self.subscription_data:
-            start_time = time.time()
-            self.data_start_time = start_time
             self.subscriber_func.append(tbkpy.Subscriber(
-                _msg, name, lambda msg: self.subscriber_msg(msg, (series_tag, start_time))
+                _msg, name, lambda msg: self.subscriber_msg(msg, series_tag)
             ))
 
     def update(self):
+        self.now_time = time.time()
         if self.is_axis_move:
-            now_time = time.time()
             dpg.fit_axis_data("plotvz_yaxis")
             dpg.fit_axis_data("plotvz_xaxis")
             dpg.set_axis_limits(
                 axis="plotvz_xaxis",
-                ymin=now_time - self.data_start_time - 10,
-                ymax=now_time - self.data_start_time
+                ymin= self.now_time - self.data_start_time - self.max_save_time,
+                ymax= self.now_time - self.data_start_time
             )
         else:
             dpg.set_axis_limits_auto("plotvz_xaxis")
