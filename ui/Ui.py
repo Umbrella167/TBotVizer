35124a59656663f75ce5459636373a43522831c2
Umbrella167 <1670187757@qq.com>
2024-11-14 17:01:58 +0800

    Finish Canvas

--- ui/Ui.py
+++ ui/Ui.py
@@ -1,6 +1,8 @@
 import dearpygui.dearpygui as dpg
 from config.UiConfig import UiConfig
 from utils.DataProcessor import ui_data
+
+
 class UICallback:
     def __init__(self):
         pass
@@ -10,19 +12,20 @@ class UICallback:
             self.config.layout.save()
             print("布局保存成功")
 
-    def on_mouse_move(self, sender, app_data):
+    def on_mouse_move(self):
         ui_data.draw_mouse_pos_last = ui_data.draw_mouse_pos
-
         ui_data.draw_mouse_pos = dpg.get_drawing_mouse_pos()
+        ui_data.mouse_move_pos = tuple(
+            x - y for x, y in zip(ui_data.draw_mouse_pos, ui_data.draw_mouse_pos_last)
+        )
 
-        ui_data.mouse_move_pos = tuple(x - y for x, y in zip(ui_data.draw_mouse_pos, ui_data.draw_mouse_pos_last))
 
-        print(ui_data.mouse_move_pos)
 class UI:
     def __init__(self, config: UiConfig):
         self.config = config
-        self.is_created =  False
+        self.is_created = False
         self._ui_callback = UICallback()
+
     def create(self):
         self.create_global_handler()
         dpg.create_viewport(title=self.config.title, width=1920, height=1080)
@@ -33,8 +36,6 @@ class UI:
             load_init_file=True,
         )
 
-        # 原show_ui部分
-        # self.config.layout.load()
         dpg.setup_dearpygui()
         dpg.show_viewport()
         self.is_created = True
@@ -42,11 +43,14 @@ class UI:
     def create_global_handler(self):
         with dpg.handler_registry() as global_hander:
             dpg.add_key_release_handler(callback=self._ui_callback.on_key_release)
-            dpg.add_mouse_drag_handler(callback=self._ui_callback.on_mouse_move)
+            # dpg.add_mouse_move_handler(callback=self._ui_callback.on_mouse_move)
+    def ui_loop(self):
+        self._ui_callback.on_mouse_move()
 
     def run_loop(self, func=None):
         if func is not None:
             while dpg.is_dearpygui_running():
+                self.ui_loop()
                 func()
                 dpg.render_dearpygui_frame()
         else:
