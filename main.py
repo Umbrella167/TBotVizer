b65438917f53571f0292fbd7c79511d06888fab7
Umbrella167 <1670187757@qq.com>
2024-11-15 13:01:26 +0800

    stage

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from utils.DataProcessor import TBKData
 from ui.boxes.PlotVzBox import PlotVzBox
 from ui.boxes.CanvasBox import CanvasBox
 
+
 def loop(UI):
     try:
         UI.update()
@@ -22,30 +23,17 @@ def loop(UI):
 
 def main():
     dpg.create_context()
-
     tbk_api = TBKApi.TBKApi()
     tbk_data = TBKData(tbk_api)
-
     t_pbox = ParamBox(tbk_data=tbk_data)
     t_msgbox = MessageBox(tag="test", tbk_data=tbk_data)
     t_canvas_box = CanvasBox()
-    t_plot_vz_box = PlotVzBox(tag="plot_vz_window")
-
-
+    t_plot_vz_box = PlotVzBox(tag="PlotVzBox")
     l = ["Option 1", "Option 2", "Option 3", "Option 4"]
     t_cbd = ComboBoxDemo(data=l)
-
-    boxes = [t_pbox, t_msgbox, t_plot_vz_box, t_cbd, t_canvas_box, PlotVzBox(), PlotVzBox(), PlotVzBox()]
-
+    boxes = [t_pbox, t_msgbox, t_plot_vz_box, t_cbd, t_canvas_box]
     UI = ui.UI(boxes=boxes)
-
-
-    
-
-
     UI.show()
-    # UI.create()
-
     UI.run_loop(lambda: loop(UI))
 
 if __name__ == "__main__":
--- ui/boxes/MessageBox.py
+++ ui/boxes/MessageBox.py
@@ -1,30 +1,37 @@
 import dearpygui.dearpygui as dpg
 from ui.boxes import Box
-
-
+from logger.Logger import Logger
+class MessageBoxCallBack:
+    def __init__(self):
+        self.subscriber_list = {}
+        pass
+    def checkbox_record_msg(self, sender, app_data, user_data):
+        msg_info,logger = user_data
 class MessageBox(Box):
     def __init__(self, tbk_data, **kwargs):
         super().__init__(**kwargs)
         self.tags = None
         self.tree_tag = None
         self.tbk_data = tbk_data
-        
+        self._callback = MessageBoxCallBack()
+        self._logger = Logger()
     def create(self):
         self.check_and_create_window()
-
         if self.label is None:
             dpg.configure_item(self.tag, label="Message")
-        # 添加列表头
-        self.tree_tag = dpg.add_collapsing_header(label="Message List", parent=self.tag)
-        # 插入树
-        self.tags = self.insert_tree(self.tbk_data.message_tree["pubs"])
+        with dpg.child_window(parent=self.tag) as self.msg_child_window_tag:
+            # 添加列表头
+            self.tree_tag = dpg.add_collapsing_header(label="Message List")
+            # 插入树
+            self.tags = self.insert_tree(self.tbk_data.message_tree["pubs"])
+
 
 
     def insert_tree(self, data):
         t_tree = []
         for puuid in data:
             # 添加节点列表
-            node = dpg.add_tree_node(label=puuid, parent=self.tree_tag)
+            node = dpg.add_tree_node(label=puuid)
             t_node = []
             for uuid in data[puuid]:
                 msg_info = data[puuid][uuid].ep_info
@@ -33,7 +40,7 @@ class MessageBox(Box):
                 msg_name = msg_info.msg_name
                 msg_type = msg_info.msg_type
                 msg_type_url = msg_info.msg_type_url
-                user_data = {
+                msg_info_dict = {
                     "msg_name": msg_name,
                     "name": name,
                     "msg_type": msg_type,
@@ -41,10 +48,10 @@ class MessageBox(Box):
                     "puuid": puuid
                 }
                 # 添加选框
-                checkbox = dpg.add_checkbox(label=f"{msg_name}({name})", parent=node)
+                checkbox = dpg.add_checkbox(label=f"{msg_name}({name})", parent=node,callback=self._callback.checkbox_record_msg, user_data=(msg_info_dict,self._logger))
                 t_node.append(checkbox)
                 # 设置拖拽
-                with dpg.drag_payload(parent=checkbox, payload_type="plot_data", drag_data=user_data):
+                with dpg.drag_payload(parent=checkbox, payload_type="plot_data", drag_data=msg_info_dict):
                     dpg.add_text(f"{puuid}_payload")
             t_tree.append(t_node)
         return t_tree
@@ -52,41 +59,3 @@ class MessageBox(Box):
 
     def update(self):
         pass
-        # dpg.does_alias_exist()
-
-        # message_data = self.tbk_data.message_data
-        # pubs = message_data["pubs"]
-        # message_node_tree = utils.build_message_tree(pubs)
-        # for puuid, node_name in message_node_tree.items():
-        #     dpg.configure_item(label=publisher,tag=f"{dpg.generate_uuid()}_treenode")
-        #     # print(dpg.get_item_configuration(f"{puuid}_treenode"))
-
-        # message_data = self.tbk_data.message_data
-        # pubs = message_data["pubs"]
-        # item = []
-        # message_node_tree = utils.build_message_tree(pubs)
-        # for puuid, node_name in message_node_tree.items():
-        #     for theme_name, node_msgs in node_name.items():
-        #         print(f"{puuid}_treenode:", dpg.get_item_configuration(f"{puuid}_treenode"))
-        #         for publisher, messages in node_msgs.items():
-        #             # f"{puuid}_{publisher}_treenode"
-        #             print(f"{puuid}_{publisher}_treenode:", dpg.get_item_configuration(f"{puuid}_{publisher}_treenode"))
-        #
-        #             for msg in messages:
-        #                 # f"{puuid}_{publisher}_{msg}_group"
-        #                 print(f"{puuid}_{publisher}_{msg}_group", dpg.get_item_configuration(f"{puuid}_{publisher}_{msg}_group"))
-        #
-        #                 uuid = f"{puuid}_{publisher}_{msg}_group"
-        #                 # print(uuid)
-        #                 dpg.set_item_user_data(f"{uuid}_checkbox", (msg, uuid))
-
-        # dpg.add_spacer(_width=80)
-        # dpg.add_text(tag=f"{uuid}_text", default_value="")
-
-        # exit(0)
-
-        # message_data = self.tbk_data.message_data
-        # pubs = message_data["pubs"]
-        # with dpg.collapsing_header(label="Message List", tag=f"{dpg.generate_uuid()}_treenode"):
-        #     item = []
-        #     message_node_tree = utils.build_message_tree(pubs)
