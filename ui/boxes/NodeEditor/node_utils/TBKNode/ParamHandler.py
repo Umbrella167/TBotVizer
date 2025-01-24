15490750e486b2ca0dcee284cf9832597a33ee06
TnTeQAQ <tnteqaq@gmail.com>
2025-01-24 13:43:48 +0800

    add: Node, ParamHandler

--- ui/boxes/NodeEditor/node_utils/TBKNode/ParamHandler.py
+++ ui/boxes/NodeEditor/node_utils/TBKNode/ParamHandler.py
@@ -1,3 +1,5 @@
+import time
+
 from ui.boxes.NodeEditor.node_utils import BaseNode
 from ui.components.TBKManager.ParamData import ParamData
 
@@ -5,20 +7,49 @@ from ui.components.TBKManager.ParamData import ParamData
 class ParamHandler(BaseNode):
     def __init__(self, **kwargs):
         default_init_data = {
-            "prefix": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": "MOTOR_CONTROL"}},
-            "name": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": "RPM"}},
-            "type": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": "list"}},
-            "info": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": "list"}},
-            "value": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": "list"}},
+            "prefix": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": None}},
+            "name": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": None}},
+            "type": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": None}},
+            "info": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": None}},
+            "value": {"attribute_type": "INPUT", "data_type": "STRINPUT", "user_data": {"value": None}},
         }
         kwargs["init_data"] = kwargs.get("init_data") or default_init_data
         super().__init__(**kwargs)
-        # self.param_data = ParamData(prefix, name, type="Unknown", info="Custom param", default_value="None"))
+        self.param_data = None
+        self.old_param_data = None
         self.automatic = True
         self.is_create = False
+        self.stabilization_flag_data = None
+        # 防抖相关属性
+        self.last_update_time = 0
+        self.DEBOUNCE_DELAY = 1
 
     def func(self):
-        prefix = self.data["name"]["user_data"].get("value")
-        name = self.data["msg_name"]["user_data"].get("value")
-        type = self.data["msg_type"]["user_data"].get("value")
-        value = self.data["msg"]["user_data"].get("value")
+        prefix = self.data["prefix"]["user_data"].get("value")
+        name = self.data["name"]["user_data"].get("value")
+        type = self.data["type"]["user_data"]["value"]
+        info = self.data["info"]["user_data"]["value"]
+        value = self.data["value"]["user_data"]["value"]
+
+        # 如果不一样说明更新了
+        if self.stabilization_flag_data != (prefix, name, type, info, value):
+            self.last_update_time = time.time()
+        self.stabilization_flag_data = (prefix, name, type, info, value)
+        if time.time() - self.last_update_time < self.DEBOUNCE_DELAY:
+            return
+        self._process_param_data(prefix, name, type, info, value)
+
+    def _process_param_data(self, prefix, name, type, info, value):
+        if all([prefix, name]):
+            self.param_data = ParamData(prefix=prefix, name=name)
+
+        if self.old_param_data != (self.param_data.type, self.param_data.info, self.param_data.value):
+            self.data["type"]["user_data"]["value"] = self.param_data.type
+            self.data["info"]["user_data"]["value"] = self.param_data.info
+            self.data["value"]["user_data"]["value"] = self.param_data.value
+        elif self.old_param_data != (type, info, value):
+            self.param_data.type = type
+            self.param_data.info = info
+            self.param_data.value = value
+
+        self.old_param_data = (type, info, value)
--- ui/components/TBKManager/ParamData.py
+++ ui/components/TBKManager/ParamData.py
@@ -1,23 +1,24 @@
 import os
 from .TBKManager import tbk_manager
 
-# TODO: 缺少删除逻辑
 
 class ParamData:
-    def __init__(self, prefix, name, type="Unknown", info="Custom param", default_value="None"):
+    def __init__(self, prefix, name, type=None, info=None, default_value=None):
         self.tbk_manager = tbk_manager
         self.prefix = prefix
         self.name = name
-        self.type = type or self.tbk_manager.get(param=os.path.join(self.prefix, self.name, "__t__"))
-        self.info = info or self.tbk_manager.get(param=os.path.join(self.prefix, self.name, "__i__"))
-        self.value = default_value or self.tbk_manager.get(param=os.path.join(self.prefix, self.name, "__v__"))
+
+        # 使用新的属性访问方法
+        self._type = type or self.tbk_manager.get(param=os.path.join(self.prefix, self.name, "__t__")) or "Unknown"
+        self._info = info or self.tbk_manager.get(param=os.path.join(self.prefix, self.name, "__i__")) or "Custom param"
+        self._value = default_value or self.tbk_manager.get(param=os.path.join(self.prefix, self.name, "__v__")) or "None"
+
         self.init_param()
 
     def init_param(self):
         self.tbk_manager.put(param=os.path.join(self.prefix, self.name, "__v__"), value=self._value)
-        self.tbk_manager.put(param=os.path.join(self.prefix, self.name, "__t__"), value=self.type)
-        self.tbk_manager.put(param=os.path.join(self.prefix, self.name, "__i__"), value=self.info)
-
+        self.tbk_manager.put(param=os.path.join(self.prefix, self.name, "__t__"), value=self._type)
+        self.tbk_manager.put(param=os.path.join(self.prefix, self.name, "__i__"), value=self._info)
 
     def __call__(self):
         return self._value
@@ -31,3 +32,23 @@ class ParamData:
     def value(self, value):
         self._value = value
         self.tbk_manager.put(param=os.path.join(self.prefix, self.name, "__v__"), value=self._value)
+
+    @property
+    def type(self):
+        self._type = self.tbk_manager.get(param=os.path.join(self.prefix, self.name, "__t__"))
+        return self._type
+
+    @type.setter
+    def type(self, type_value):
+        self._type = type_value
+        self.tbk_manager.put(param=os.path.join(self.prefix, self.name, "__t__"), value=self._type)
+
+    @property
+    def info(self):
+        self._info = self.tbk_manager.get(param=os.path.join(self.prefix, self.name, "__i__"))
+        return self._info
+
+    @info.setter
+    def info(self, info_value):
+        self._info = info_value
+        self.tbk_manager.put(param=os.path.join(self.prefix, self.name, "__i__"), value=self._info)
